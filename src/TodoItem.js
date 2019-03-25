c7668d4a6866232c7a507e7a3d9e1b0e7da60943
Przemysław Adamczyk <ppadamczyk257@gmail.com>
2019-03-25 19:05:33 +0000

    Todos can be now completed which makes them crossed off

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const TodoItem = ({name, completed, onDelete}) => (
+const TodoItem = ({name, completed, onDelete, onToggle}) => (
     <li
     style={{
         textDecoration: completed? 'line-through' : 'none'
     }}
-    >
+    ><span onClick={onToggle}>
         {name}
+    </span>
         <span onClick={onDelete}> X </span>
     </li>
 );
