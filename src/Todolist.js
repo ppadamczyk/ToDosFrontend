4769f21b3c1c03b36bb81da8572acbfc05ad5f42
Przemysław Adamczyk <ppadamczyk257@gmail.com>
2019-03-25 10:48:52 +0000

    Loading toDos from API with error handling added

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,16 +1,40 @@
 import React, {Component} from 'react';
+const APIURL='/api/todos';
 
 class Todolist extends Component{
     constructor(props){
         super(props);
         this.state = {
             todos:[]
-        }
+        };
+    }
+    
+    componentWillMount(){
+        this.loadToDos();
+    }
+    
+    loadToDos(){
+        fetch(APIURL)
+        .then(resp => {
+            if(!resp.ok) {
+                if(resp.status >= 400 && resp.status < 500){
+                    return resp.json().then(data => {
+                        let err = {errorMessage: data.message};
+                        throw err;
+                    });
+                } else {
+                    let err = {errorMessage: 'Please try again later'};
+                    throw err;
+                }
+            }
+            return resp.json();
+        })
+        .then(todos => this.setState({todos}));
     }
     render(){
         return(
             <h1>Todo list!</h1>
-            )
+            );
     }
 }
 
